3c9915f2851b2a5b78515f45c2627a4c8017eb43
Marc Portier <marc.portier@gmail.com>
2015-02-02 09:32:59 +0100

    we don't need javascript clientside to get the languageLinks correct

--- js/main.js
+++ js/main.js
@@ -1,21 +1,6 @@
 ;
 (function($) {
     $(function(){
-        var $langs = $('#lang li a')
-          , $activeMenu = $('#menu li .active')
-          , langLinks = $activeMenu.data('langlinks')
-        ;
 
-        $langs.each(function(){
-            var $this = $(this)
-              , lang = $this.data('lang')
-              , link = langLinks[lang].link
-            ;
-
-            if (link) {
-                $this.attr('href', link );
-            }
-
-        });
     });
 })(jQuery);
