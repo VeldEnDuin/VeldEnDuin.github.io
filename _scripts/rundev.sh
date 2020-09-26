7176effde8ba7bcb5a1d5165e614b4e6f2a386e1
Marc Portier <marc.portier@gmail.com>
2020-09-26 16:36:46 +0200

    some extra features & description for ./rundev script

--- _scripts/rundev.sh
+++ _scripts/rundev.sh
@@ -8,4 +8,21 @@ while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
 done
 DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 
-(cd $DIR/.. && bundle exec jekyll serve --port 4444 -w --incremental)
+if [[ "$1" == "help" || "$1" == "-h" || "$1" == "--help" ]]; then
+  echo "$0 help  --> display this page" 
+  echo "$0 clean --> run the site but clean the build first" 
+  echo "$0 slow  --> avoid incremental builds // rebuild is slower" 
+  echo "$0       --> just run the site in default incremental mode" 
+  exit 0
+fi
+
+if [[ "$1" == "clean" ]]; then
+  (cd $DIR/.. && rm -rf ./_site) 
+fi
+
+flags="--incremental" #normal mode
+if [[ "$1" == "slow" ]]; then
+  flags=""  
+fi
+
+(cd $DIR/.. && bundle exec jekyll serve --port 4444 -w ${flags})
