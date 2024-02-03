da107f82c397ee154aa1753e56cd3368fe83a98f
Marc Portier <marc.portier@gmail.com>
2024-02-03 09:01:23 +0100

    make sure to bind to all local ports for test-sharing

--- _scripts/rundev.sh
+++ _scripts/rundev.sh
@@ -26,4 +26,4 @@ if [[ "$1" == "slow" ]]; then
 fi
 
 echo "flags == ${flags}"
-(cd $DIR/.. && bundle exec jekyll serve --port 4444 --livereload --livereload-port 40044 ${flags})
+(cd $DIR/.. && bundle exec jekyll serve --host 0.0.0.0 --port 4444 --livereload --livereload-port 40044 ${flags})
